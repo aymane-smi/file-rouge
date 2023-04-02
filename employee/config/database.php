7cf8372c1974966e4bddbfff1f1a07fec307ed3e
aymane-smi <aymanebel2@outlook.fr>
2023-04-02 15:01:30 +0000

    adding volumes to services and making migration for employee

--- employee/config/database.php
+++ employee/config/database.php
@@ -65,7 +65,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
+            // 'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
