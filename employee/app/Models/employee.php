2d7e93d421a855f9031b1129e3dcceba2f5345be
aymane-smi <aymanebel2@outlook.fr>
2023-04-02 17:23:13 +0000

    making validators for input and crypting the password while creating new employee

--- employee/app/Models/employee.php
+++ employee/app/Models/employee.php
@@ -10,4 +10,8 @@ class employee extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function setPasswordAttribute($password){
+        $this->attributes["password"] = bcrypt($password);
+    }
 }
