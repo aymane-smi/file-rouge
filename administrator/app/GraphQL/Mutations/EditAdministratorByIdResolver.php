6f4c4555ac42176bbc0b65789e1076c44865d1da
aymane-smi <aymanebel2@outlook.fr>
2023-04-03 14:46:09 +0000

    making a single database instance.making administrator resolvers

---
+++ administrator/app/GraphQL/Mutations/EditAdministratorByIdResolver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\administrator;
+use Illuminate\Support\Facades\DB;
+
+final class EditAdministratorByIdResolver
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+
+        DB::table("employees")->where("id", $args["id"])->update($args);
+        $tmp = administrator::find($args["id"]);
+
+        return $tmp;
+    }
+}
