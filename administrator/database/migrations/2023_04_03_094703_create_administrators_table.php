01432f581f0a44965b4e79b7ad690ae8c66d8cde
aymane-smi <aymanebel2@outlook.fr>
2023-04-03 15:47:41 +0000

    making administrator seeder and factory

--- administrator/database/migrations/2023_04_03_094703_create_administrators_table.php
+++ administrator/database/migrations/2023_04_03_094703_create_administrators_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("first_name");
             $table->string("last_name");
-            $table->string("email");
+            $table->string("email")->unique();
             $table->string("password");
             $table->timestamps();
         });
--- administrator/database/seeders/DatabaseSeeder.php
+++ administrator/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\administrator;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        administrator::truncate();
+        administrator::factory(1)->create();
     }
 }
