bb1b7ed34394c7ef2b7d5a33d95e533ae67e1700
aymane-smi <aymanebel2@outlook.fr>
2023-04-12 01:57:44 +0000

    adding complains migration and fixing some ui

---
+++ administrator/app/GraphQL/Mutations/addComplaint.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+final class addComplaint
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+
+
+    }
+}
