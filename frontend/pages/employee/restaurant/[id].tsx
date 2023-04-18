17d461b5b5a32a13c8c130e5c14927ede8c433de
aymane-smi <aymanebel2@outlook.fr>
2023-04-18 20:51:56 +0000

    more work to do

--- frontend/pages/employee/restaurant/[id].tsx
+++ frontend/pages/employee/restaurant/[id].tsx
@@ -5,56 +5,71 @@ import styles from "../../../styles/employee_index.module.css";
 import HeaderRes from "../../../components/employee/HeaderRes";
 import {GiFullPizza} from "react-icons/gi";
 import Cart from "../../../components/employee/cart";
+import { useLazyQuery } from "@apollo/client";
+import { getRestaurantById } from "../../../utils/gql";
+import { useEffect, useState } from "react";
+import { Loading } from "../../../components/utils/Loading";
 
 export default function restaurantEmployee(){
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter();
+    const [MyQuery, {loading, data, error}] = useLazyQuery(getRestaurantById);
+    const [category, setCategory] = useState<string>("");
 
+    useEffect(()=>{
+        if(router.query.id !== undefined){
+            MyQuery({
+                variables:{
+                    id: parseInt(router.query.id),
+                }
+            });
+        }
+    }, [router]);
     return (<>
         <Head>
             <title>restaurant</title>
         </Head>
         <main className="flex w-screen">
+            {loading ? <Loading /> : <>
             <SideBar />
             <div className={["h-screen overflow-y-scroll relative", styles.width].join(" ")}>
                 <Cart />
                 <HeaderRes />
                 <div className={["p-10", styles.width].join(" ")}>
                     <div className="flex gap-5 w-full overflow-x-scroll">
-                    {[1,2,3,4,5,6,7, 8, 9, 10, 11, 12].map((item)=>(
-                        <div key={item} className={`${item == 1 ? "bg-yellow-200" : ""} w-fit p-3 h-[150px] rounded-lg flex flex-col justify-between items-center`}>
-                            <div className={`w-[80px] h-[80px] rounded-[50%] ${item == 1 ? "bg-gray-300" : "border-2"} flex justify-center items-center`}>
-                                <GiFullPizza size={50} />
+                    {data?.getRestaurantById?.categories.map((item, i)=>(
+                        <div onClick={()=>setCategory(item.name)} key={i} className={`${item == 1 ? "bg-yellow-200" : ""} w-fit p-3 h-[150px] rounded-lg flex flex-col justify-between items-center`}>
+                            <div className={`w-[80px] h-[80px] rounded-[50%] ${item. == 1 ? "bg-gray-300" : "border-2"} flex justify-center items-center`}>
+                                <img src={item.emoji} className="w-[40px] h-[40px]"/>
                             </div>
-                            <span className={`${item == 1 ? "font-bold" : ""} text-[15px]`}>all</span>
+                            <span className={`${i == 1 ? "font-bold" : ""} text-[15px]`}>
+                                {item.name}
+                            </span>
                         </div>
                     ))}
                     </div>
                 </div>
                 {/* menus */}
                 <div className="grid grid-cols-3 gap-5">
-                        {[1,2,3,4,5,6].map((item)=>(
-                            <div className=" justify-self-center place-self-center rounded-md border-[1px] border-black w-[250px] h-[250px] flex flex-col justify-center items-center gap-3 p-2" key={"menu-"+item}>
-                                <img className="rounded-md h-[120px] w-[200px]" src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
-                                <p className="text-[20px]">Pizza</p>
-                                <div className="flex gap-5">
-                                    <div className="w-fit flex flex-col justify-center items-center">
-                                        <p className="text-[15px] text-gray-400">small</p>
-                                        <p className="font-bold text-[15px]">20dh</p>
-                                    </div>
-                                    <div className="w-fit flex flex-col justify-center items-center">
-                                        <p className="text-[15px] text-gray-400">small</p>
-                                        <p className="font-bold text-[15px]">20dh</p>
-                                    </div>
-                                    <div className="w-fit flex flex-col justify-center items-center">
-                                        <p className="text-[15px] text-gray-400">small</p>
-                                        <p className="font-bold text-[12px]">20dh</p>
+                        {data?.getRestaurantById?.categories?.map((item, i)=>(
+                            item?.menus.map((item, i)=>(
+                                <div className=" justify-self-center place-self-center rounded-md border-[1px] border-black w-[250px] h-[250px] flex flex-col justify-center items-center gap-3 p-2" key={"menu-"+item}>
+                                    <img className="rounded-md h-[120px] w-[200px]" src={"http://localhost:9003/storage/menu/"+item?.image}/>
+                                    <p className="text-[20px]">{item?.menu?.name}</p>
+                                    <div className="flex gap-5">
+                                        {item?.details?.map((item, i)=>(
+                                            <div key={i} className="w-fit flex flex-col justify-center items-center">
+                                                <p className="text-[15px] text-gray-400">{item.portion}</p>
+                                                <p className="font-bold text-[15px]">{item.price}dh</p>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
-                            </div>
+                            ))
                         ))}
                 </div>
             </div>
+            </>}
         </main>
     </>);
 }
